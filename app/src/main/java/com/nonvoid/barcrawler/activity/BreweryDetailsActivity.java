c2c48586f77bef50e13f1050216cede8da9106b3
Matt Goodwin <mgoodwin14@gmail.com>
2017-05-13 14:26:04 -0400

    added empty brewery details activity

---
+++ app/src/main/java/com/nonvoid/barcrawler/activity/BreweryDetailsActivity.java
@@ -0,0 +1,11 @@
+package com.nonvoid.barcrawler.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Matt on 5/13/2017.
+ */
+
+public class BreweryDetailsActivity extends Activity {
+
+}
---
+++ app/src/main/java/com/nonvoid/barcrawler/util/IntentTags.java
@@ -0,0 +1,8 @@
+package com.nonvoid.barcrawler.util;
+
+/**
+ * Created by Matt on 5/13/2017.
+ */
+
+public class IntentTags {
+}
