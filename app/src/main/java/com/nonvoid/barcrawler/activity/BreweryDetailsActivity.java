e3d479aca4dd4981a4e84db40cb7c7cd4ee06d61
Matt Goodwin <mgoodwin14@gmail.com>
2017-05-13 17:46:43 -0400

    appbar to details, remove map fragment from list

--- app/src/main/java/com/nonvoid/barcrawler/activity/BreweryDetailsActivity.java
+++ app/src/main/java/com/nonvoid/barcrawler/activity/BreweryDetailsActivity.java
@@ -1,11 +1,9 @@
 package com.nonvoid.barcrawler.activity;
 
-import android.app.Activity;
-
 /**
  * Created by Matt on 5/13/2017.
  */
 
-public class BreweryDetailsActivity extends Activity {
+public class BreweryDetailsActivity extends BaseActivity {
 
 }
