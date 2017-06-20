2c3b0fb4f37b2d5615e17b74a9cdc041d58289e0
Matt Goodwin <mgoodwin14@gmail.com>
2017-06-20 12:49:12 -0400

    refactored

--- app/src/main/java/com/nonvoid/barcrawler/activity/MainActivity.java
+++ app/src/main/java/com/nonvoid/barcrawler/activity/MainActivity.java
@@ -1,6 +1,5 @@
 package com.nonvoid.barcrawler.activity;
 
-import android.app.Application;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,7 +8,6 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.nonvoid.barcrawler.R;
 import com.nonvoid.barcrawler.dagger.MyApp;
@@ -19,35 +17,28 @@ import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     @Inject
     SharedPreferences sharedPreferences;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment fragment;
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-//                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-//                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_search:
-                    fragment = new BreweryListFragment();
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.main_activity_content, fragment)
-                            .commit();
-                    return true;
-            }
-            return false;
-        }
-
-    };
+            = item -> {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+    //                    mTextMessage.setText(R.string.title_home);
+                        return true;
+                    case R.id.navigation_dashboard:
+    //                    mTextMessage.setText(R.string.title_dashboard);
+                        return true;
+                    case R.id.navigation_search:
+                        fragment = new BreweryListFragment();
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_activity_content, fragment)
+                                .commit();
+                        return true;
+                }
+                return false;
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,11 +47,6 @@ public class MainActivity extends AppCompatActivity {
 
         ((MyApp) getApplication()).getNetComponent().inject(this);
 
-        if(sharedPreferences != null){
-            Toast.makeText(this, "Dagger is working", Toast.LENGTH_LONG).show();
-        }else {
-            Toast.makeText(this, "Dagger is NOT working", Toast.LENGTH_LONG).show();
-        }
 
 //        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
