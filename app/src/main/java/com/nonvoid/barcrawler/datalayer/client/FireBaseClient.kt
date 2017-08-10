b955b0b3fce35fc83587152ab069e90662761cb4
Matt Goodwin <mgoodwin14@gmail.com>
2017-08-09 21:22:07 -0500

    updated firebaseclient to like and dislike a beer

--- app/src/main/java/com/nonvoid/barcrawler/datalayer/client/FireBaseClient.kt
+++ app/src/main/java/com/nonvoid/barcrawler/datalayer/client/FireBaseClient.kt
@@ -1,15 +1,12 @@
 package com.nonvoid.barcrawler.datalayer.client
 
 import android.util.Log
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.nonvoid.barcrawler.model.Beer
 import com.nonvoid.barcrawler.model.Brewery
 import durdinapps.rxfirebase2.RxFirebaseDatabase
 import io.reactivex.Maybe
-import io.reactivex.Observable
-import io.reactivex.Observer
-import java.util.*
 
 /**
  * Created by Matt on 8/8/2017.
@@ -18,9 +15,7 @@ class FireBaseClient(private val user: FirebaseUser) {
     private val reference :DatabaseReference = FirebaseDatabase.getInstance().reference
 
     fun setBreweryAsFavorite(brewery: Brewery, favorite: Boolean) {
-        getBreweryReference()
-            .child(brewery.id)
-            .child(FAVORITE)
+        getBreweryFavoriteReference(brewery)
             .child(user.uid)
             .setValue(favorite)
             .addOnCompleteListener { result ->
@@ -33,21 +28,58 @@ class FireBaseClient(private val user: FirebaseUser) {
     }
 
     fun getNumberOfFavoritesForBrewery(brewery: Brewery): Maybe<Int> {
-
-        return RxFirebaseDatabase.observeSingleValueEvent(getBreweryReference()
-                .child(brewery.id)
-                .child(FAVORITE)
+        return RxFirebaseDatabase.observeSingleValueEvent(getBreweryFavoriteReference(brewery)
                 .orderByValue()
-                .equalTo(true), {snapShot: DataSnapshot -> snapShot.childrenCount.toInt()  }
+                .equalTo(true), {snapShot -> snapShot.childrenCount.toInt()}
         )
     }
 
-    private fun getBreweryReference(): DatabaseReference{
+    fun isBeerLiked(beer: Beer): Maybe<Boolean> {
+        return RxFirebaseDatabase.observeSingleValueEvent(getBeerRatingReference(beer)
+                .child(user.uid)
+                .orderByValue(), {snapShot -> snapShot.value == 1}
+        )
+    }
+
+    fun likeBeer(beer: Beer){
+        rateBeer(beer, 1)
+    }
+
+    fun dislikeBeer(beer: Beer){
+        rateBeer(beer, 0)
+    }
+
+    private fun rateBeer(beer: Beer, rating: Int){
+        getBeerRatingReference(beer)
+                .child(user.uid)
+                .setValue(rating)
+                .addOnCompleteListener { result ->
+                    if(result.isSuccessful){
+                        Log.d("MPG", "set rating to $rating")
+                    }else {
+                        Log.d("MPG", "failed to set rating to $rating")
+                    }
+                }
+
+    }
+
+    private fun getBreweryFavoriteReference(brewery: Brewery): DatabaseReference{
         return reference.child(BREWERY)
+                .child(brewery.id)
+                .child(FAVORITE)
+    }
+
+    private fun getBeerRatingReference(beer: Beer): DatabaseReference{
+        return reference.child(BEER)
+                .child(beer.id)
+                .child(RATING)
+
     }
 
     companion object {
         const val BREWERY = "brewery"
         const val FAVORITE = "favorite"
+        const val BEER = "beer"
+        const val RATING = "rating"
     }
 }
