a94721816db035298d1b3b1f952f8e375ddcd304
Matt Goodwin <mgoodwin14@gmail.com>
2017-08-12 14:11:24 -0500

    renamed and added some tests

--- app/src/test/java/com/nonvoid/barcrawler/BreweryDetailsPresenter.kt
+++
@@ -1,5 +0,0 @@
-package com.nonvoid.barcrawler
-
-/**
- * Created by Matt on 8/12/2017.
- */
---
+++ app/src/test/java/com/nonvoid/barcrawler/BreweryDetailsPresenterTest.kt
@@ -0,0 +1,37 @@
+package com.nonvoid.barcrawler
+
+import com.nonvoid.barcrawler.brewery.BreweryDetailsPresenter
+import com.nonvoid.barcrawler.model.Brewery
+import com.nonvoid.barcrawler.social.SocialRepoAPI
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+/**
+ * Created by Matt on 8/12/2017.
+ */
+class BreweryDetailsPresenterTest {
+    val view = Mockito.mock(BreweryDetailsPresenter.BreweryDetailsView::class.java)
+    val brewery = Mockito.mock(Brewery::class.java)
+    val socialClient = Mockito.mock(SocialRepoAPI::class.java)
+    val subject = BreweryDetailsPresenter(view, brewery, socialClient)
+
+    @Before
+    fun setUp(){
+        Mockito.`when`(socialClient.isBreweryFavorited(brewery))
+                .thenReturn(Single.just(true))
+        Mockito.`when`(socialClient.getNumberOfFavoritesForBrewery(brewery))
+                .thenReturn(Single.just(1))
+
+        Mockito.doNothing().`when`(socialClient).favoriteBrewery(brewery)
+        Mockito.doNothing().`when`(socialClient).unfavoriteBrewery(brewery)
+    }
+
+    @Test
+    fun onCreate_success(){
+        subject.onCreate()
+        Mockito.verify(view).displayBrewery(brewery)
+        Mockito.verify(socialClient).getNumberOfFavoritesForBrewery(brewery)
+    }
+}
