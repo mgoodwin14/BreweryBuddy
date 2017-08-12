beba8c4c1f69e82229b5bb39c42a241c111739d7
Matt Goodwin <mgoodwin14@gmail.com>
2017-08-12 01:50:54 -0500

    created BreweryDetailsPresenter

---
+++ app/src/main/java/com/nonvoid/barcrawler/brewery/BreweryDetailsPresenter.kt
@@ -0,0 +1,50 @@
+package com.nonvoid.barcrawler.brewery
+
+import com.google.firebase.auth.FirebaseAuth
+import com.nonvoid.barcrawler.model.Brewery
+import com.nonvoid.barcrawler.social.FireBaseSocialClient
+import com.nonvoid.barcrawler.social.SocialRepoAPI
+import android.R.id.edit
+import android.content.SharedPreferences
+import android.view.MenuItem
+import javax.inject.Inject
+
+
+/**
+ * Created by Matt on 8/11/2017.
+ */
+class BreweryDetailsPresenter(
+        private val view: BreweryDetailsView,
+        private val brewery: Brewery,
+        private val socialClient: SocialRepoAPI = FireBaseSocialClient(FirebaseAuth.getInstance().currentUser!!)
+){
+
+    fun onCreate(){
+        view.displayBrewery(brewery)
+        socialClient.getNumberOfFavoritesForBrewery(brewery)
+                .subscribe({result -> view.displayFavoriteCount(result)})
+    }
+
+    fun setFavoriteMenuItem(){
+        socialClient.isBreweryFavorited(brewery)
+                .subscribe({result -> view.displayAsFavorite(result)})
+    }
+
+    fun toggleFavorite(){
+        socialClient.isBreweryFavorited(brewery)
+                .subscribe({result-> setBreweryAsFavorite(!result)})
+    }
+
+    private fun setBreweryAsFavorite(favorite: Boolean){
+        socialClient.setBreweryAsFavorite(brewery, favorite)
+        view.displayAsFavorite(favorite)
+        socialClient.getNumberOfFavoritesForBrewery(brewery)
+                .subscribe({result -> view.displayFavoriteCount(result)})
+    }
+
+    interface BreweryDetailsView{
+        fun displayBrewery(brewery :Brewery)
+        fun displayAsFavorite(favorite :Boolean)
+        fun displayFavoriteCount(count :Int)
+    }
+}
