0b10bbc295743e9ef5f820ec8cf5edcf22a40ba8
Matt Goodwin <mgoodwin14@gmail.com>
2017-08-12 17:26:51 -0500

    made HomeActivity 'dumb' using presenter

--- app/src/main/java/com/nonvoid/barcrawler/HomeActivity.kt
+++ app/src/main/java/com/nonvoid/barcrawler/HomeActivity.kt
@@ -1,6 +1,5 @@
 package com.nonvoid.barcrawler
 
-import android.app.AlertDialog
 import android.app.ProgressDialog
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
@@ -10,29 +9,31 @@ import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
 
 import com.nonvoid.barcrawler.dagger.MyApp
 import com.nonvoid.barcrawler.beer.BeerListFragment
 import com.nonvoid.barcrawler.brewery.BreweryListFragment
 import com.nonvoid.barcrawler.brewery.BreweryLocationFragment
-import durdinapps.rxfirebase2.RxFirebaseAuth
-import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.app_bar_nav_drawer.*
-import android.net.NetworkInfo
-import android.content.Context.CONNECTIVITY_SERVICE
 import android.net.ConnectivityManager
+import android.view.inputmethod.InputMethodManager
+import com.nonvoid.barcrawler.database.BreweryDataBaseAPI
+import com.nonvoid.barcrawler.model.Beer
+import com.nonvoid.barcrawler.model.Brewery
+import com.nonvoid.barcrawler.model.BreweryLocation
+import javax.inject.Inject
 
 
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, HomePresenter.HomeView, SearchFragment.Searchable {
 
-class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    @Inject
+    lateinit var client: BreweryDataBaseAPI
 
-    val disposables : CompositeDisposable = CompositeDisposable()
+    lateinit var presenter : HomePresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,31 +42,15 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setSupportActionBar(toolbar)
         (application as MyApp).netComponent.inject(this)
 
-        val firebaseAuth = FirebaseAuth.getInstance()
-
-        if(firebaseAuth.currentUser!=null){
-            firebaseAuth.currentUser!!.reload()
-            Log.d("MPG", "currentUser: ${firebaseAuth.currentUser}")
-        }else {
-            val dialog = ProgressDialog.show(this, "Logging in", "Please wait.", true)
-            RxFirebaseAuth.signInAnonymously(firebaseAuth)
-                    .doFinally { dialog.dismiss() }
-                    .subscribe({ result ->
-                        if (result.user != null) {
-                            Toast.makeText(this, "Signed in as anonymous user", Toast.LENGTH_LONG).show()
-                        } else {
-                            Toast.makeText(this, "Sign in failed", Toast.LENGTH_LONG).show()
-                        }
-                    })
-        }
+        presenter = HomePresenter(this, client)
+        presenter.onCreate()
 
         setUpDrawerNav()
-        val breweryFragment = BreweryListFragment()
-        val searchFragment = SearchFragment.newInstance(breweryFragment)
-        supportFragmentManager.beginTransaction()
-                .add(R.id.search_fragment_frame_layout, searchFragment, searchFragment.javaClass.simpleName)
-                .add(R.id.content_frame_layout, breweryFragment, breweryFragment.javaClass.simpleName)
-                .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.nav_drawer, menu)
+        return true
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -90,15 +75,6 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return false
     }
 
-    fun replaceContent(fragment: Fragment){
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.content_frame_layout, fragment)
-                .commit()
-
-        val searchFragment = supportFragmentManager.findFragmentByTag(SearchFragment::class.java.simpleName) as SearchFragment
-        searchFragment.setSearchable(fragment as SearchFragment.Searchable)
-    }
-
     override fun onBackPressed() {
         if(drawer_layout.isDrawerOpen(GravityCompat.START))
             drawer_layout.closeDrawer(GravityCompat.START)
@@ -108,15 +84,67 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             super.onBackPressed()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.nav_drawer, menu)
-        return true
+    override fun addSearchFragment() {
+        val searchFragment = SearchFragment()
+        searchFragment.setSearchable(this)
+        val breweryFragment = BreweryListFragment()
+        supportFragmentManager.beginTransaction()
+                .add(R.id.search_fragment_frame_layout, searchFragment, searchFragment.javaClass.simpleName)
+                .add(R.id.content_frame_layout, breweryFragment, breweryFragment.javaClass.simpleName)
+                .commit()
     }
 
-    override fun onPause() {
-        super.onPause()
-        disposables.clear()
+    override fun doOnSearch(query: String) {
+
+        val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(window.decorView.windowToken, 0)
+
+        supportFragmentManager.findFragmentByTag(BreweryListFragment::class.java.simpleName)?.let {
+            presenter.brewerySearch(query)
+            return
+        }
+        supportFragmentManager.findFragmentByTag(BeerListFragment::class.java.simpleName)?.let {
+            presenter.beerSearch(query)
+            return
+        }
+        supportFragmentManager.findFragmentByTag(BreweryLocationFragment::class.java.simpleName)?.let {
+            presenter.locationSearch(query)
+            return
+        }
+    }
+
+    override fun displayBreweryList(breweryList: List<Brewery>) {
+        val breweryFragment = BreweryListFragment.newInstance(ArrayList(breweryList))
+        replaceContent(breweryFragment)
+    }
+
+    override fun displayLocationFragment(locationList: List<BreweryLocation>) {
+        val locationFragment = BreweryLocationFragment.newInstance(ArrayList(locationList))
+    }
+
+    override fun displayBeerFragment(beerList: List<Beer>) {
+        val beerFragment = BeerListFragment.newInstance(ArrayList(beerList))
+        replaceContent(beerFragment)
+    }
+
+    override fun showDialog(title: String, message: String): ProgressDialog {
+        return ProgressDialog.show(this, title, message, true)
+    }
+
+    override fun makeToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun replaceContent(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.content_frame_layout, fragment, fragment::class.java.simpleName)
+                .commit()
+    }
+
+    private fun isNetworkVailable():Boolean{
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 
     private fun setUpDrawerNav() {
@@ -125,11 +153,5 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
         nav_view.setNavigationItemSelectedListener(this)
-    }
-
-    private fun isNetworkVailable():Boolean{
-        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetworkInfo = connectivityManager.activeNetworkInfo
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 }
