2b7ee25670625351621683c571c2f08c984e9e68
Matt Goodwin <mgoodwin14@gmail.com>
2017-08-17 08:26:28 -0500

    created a builder for BreweryDetailsPresenter

--- app/src/main/java/com/nonvoid/barcrawler/brewery/BreweryDetailsPresenter.kt
+++ app/src/main/java/com/nonvoid/barcrawler/brewery/BreweryDetailsPresenter.kt
@@ -5,24 +5,27 @@ import com.nonvoid.barcrawler.model.Brewery
 import com.nonvoid.barcrawler.social.FireBaseSocialClient
 import com.nonvoid.barcrawler.social.SocialRepoAPI
 import com.nonvoid.barcrawler.database.BreweryDataBaseAPI
+import com.nonvoid.barcrawler.database.BreweryDataBaseClient
 import com.nonvoid.barcrawler.model.Beer
 
 class BreweryDetailsPresenter(
-        private val view: BreweryDetailsView,
+        private val detailsView: BreweryDetailsView,
+        private val listView: BeerListView,
         private val brewery: Brewery,
         private val dbClient: BreweryDataBaseAPI,
         private val socialClient: SocialRepoAPI = FireBaseSocialClient(FirebaseAuth.getInstance().currentUser!!)
 ){
 
     fun onCreate(){
-        view.displayBrewery(brewery)
+        detailsView.displayBrewery(brewery)
+        getBeerList()
         socialClient.getNumberOfFavoritesForBrewery(brewery)
-                .subscribe({result -> view.displayFavoriteCount(result)})
+                .subscribe({result -> detailsView.displayFavoriteCount(result)})
     }
 
     fun setFavoriteMenuItem(){
         socialClient.isBreweryFavorited(brewery)
-                .subscribe({result -> view.displayAsFavorite(result)})
+                .subscribe({result -> detailsView.displayAsFavorite(result)})
     }
 
     fun toggleFavorite(){
@@ -32,7 +35,7 @@ class BreweryDetailsPresenter(
 
     fun getBeerList(){
         dbClient.getBeersForBrewery(brewery.id)
-                .subscribe{list -> view.displayBeerList(list)}
+                .subscribe{list -> listView.displayBeerList(list)}
     }
 
     private fun setBreweryAsFavorite(favorite: Boolean){
@@ -41,15 +44,55 @@ class BreweryDetailsPresenter(
         }else {
             socialClient.unfavoriteBrewery(brewery)
         }
-        view.displayAsFavorite(favorite)
+        detailsView.displayAsFavorite(favorite)
         socialClient.getNumberOfFavoritesForBrewery(brewery)
-                .subscribe({result -> view.displayFavoriteCount(result)})
+                .subscribe({result -> detailsView.displayFavoriteCount(result)})
     }
 
     interface BreweryDetailsView{
         fun displayBrewery(brewery :Brewery)
-        fun displayBeerList(list: List<Beer>)
         fun displayAsFavorite(favorite :Boolean)
         fun displayFavoriteCount(count :Int)
+    }
+
+    interface BeerListView{
+        fun displayBeerList(list: MutableList<Beer>)
+    }
+
+    class Builder{
+        private lateinit var detailsView: BreweryDetailsView
+        private lateinit var listView: BeerListView
+        private lateinit var brewery: Brewery
+        private lateinit var dbClient: BreweryDataBaseAPI
+        private lateinit var socialClient: SocialRepoAPI
+
+        fun detailsView(view: BreweryDetailsView) :Builder{
+            this.detailsView = view
+            return this
+        }
+
+        fun listView(view: BeerListView) :Builder{
+            this.listView = view
+            return this
+        }
+
+        fun brewery(brewery: Brewery) :Builder{
+            this.brewery = brewery
+            return this
+        }
+
+        fun dbClient(dbClient: BreweryDataBaseAPI) :Builder{
+            this.dbClient = dbClient
+            return this
+        }
+
+        fun socielClient(socialClient: SocialRepoAPI) :Builder{
+            this.socialClient = socialClient
+            return this
+        }
+
+        fun build(): BreweryDetailsPresenter{
+            return BreweryDetailsPresenter(detailsView, listView, brewery, dbClient, socialClient)
+        }
     }
 }
